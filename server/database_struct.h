05332a744cde59a6a58447183004ce5becd54b57
mathilda <fbr.ma@outlook.com>
2024-02-26 17:37:37 +0800

    20240224 fbr add database_struct.h

--- server/database_struct.h
+++ server/database_struct.h
@@ -1,5 +1,6 @@
 #ifndef _DATABASE_STRUCT_H_
 #define _DATABASE_STRUCT_H_
+#include <stddef.h>
 
 typedef struct {
     int userid;         // 用户id
@@ -22,4 +23,26 @@ typedef struct {
     int tomb;           // 墓碑值
 } File;
 
+// 用户相关
+int dbAddUser(const User* user);                       // 添加用户
+User* dbFindUserByName(const char* username);          // 通过用户名查找用户
+int dbDeleteUser(int userid);                          // 删除用户
+
+// 文件相关
+int dbAddFile(const File* file);                       // 添加文件记录
+File* dbFindFileBySHA1(const char* sha1);              // 通过SHA1查找文件
+File* dbFindFileByDirID(int directoryId, int userid); // 通过文件id从数据库查找目录下的文件
+File** dbListFileByParentID(int preid, int userid); /*通过文件的父亲的id找到所有文件，ls命令将调用这个函数。
+这个函数会返回一个File结构体指针的数组，以NULL空指针结尾。
+*/
+
+/**
+ * 根据给定的普通文件名查询其SHA1值。
+ * 
+ * @param filename 普通文件名。
+ * @param sha1Value 指向预先分配足够空间的字符串指针，用于存放查询到的SHA1值。
+ * @param sha1ValueSize sha1Value字符串分配的大小。
+ * @return 0 表示成功，非0表示失败代码。
+ */
+int dbFindSHA1ByFilename(const char* filename, char* sha1Value, size_t sha1ValueSize);
 #endif
