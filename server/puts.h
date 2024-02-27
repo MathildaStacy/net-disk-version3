43b1df632f09dbe5a2413931a3e018506229af6d
mayaoxi <1970851948@qq.com>
2024-02-27 12:00:57 +0800

    2024/2/27 11:56 update puts.h

--- server/puts.h
+++ server/puts.h
@@ -1,6 +1,7 @@
 #ifndef _PUTS_H_
 #define _PUTS_H_
-
+#include <mysql/mysql.h>
+#include "pwd.h"
 int msgtrans(char *msg,int sockfd);
 
 int msgrecv(char* msg , int sockfd);
@@ -11,7 +12,7 @@ int putsfile(char* name , int netfd,long offset);
 int getfile(int sockfd,int opfd ,long offset);
 
 int commandPuts_C(char* filename,int sockfd);
-int commandPuts_S(int sockfd);
+int commandPuts_S(MYSQL * conn,dirStackType*virtual_path,int sockfd);
 
 #endif
 
