76a4a93a49097769b8bc96aab5518caa30ab6b1a
mathilda <fbr.ma@outlook.com>
2024-02-28 14:15:49 +0800

    20240228 fbr rm re[eclate files.

--- server/fbr_gets_and_puts.h
+++
@@ -1,9 +0,0 @@
-#ifndef _FBR_GETS_AND_PUTS_H_
-#define _FBR_GETS_AND_PUTS_H_
-
-
-int sendFile(int sockfd, const char*sha1); //服务端  参数1：客户端的netfd
-
-int recvFile(int sockfd, const char *sha1);
-
-#endif
--- server/logger.c
+++ server/logger.c
@@ -1,19 +1,8 @@
-#ifndef __LOGGER_H
-#define __LOGGER_H
+#include "logger.h"
 
-#include <stdio.h>
-#include <time.h>
-#include <pthread.h>
-#include <stdarg.h>
-
-#define LOG_LEVEL_DEBUG 0  //日志级别，如果设置级别为0则所有级别大于等于0的级别的日志都可以打印
-#define LOG_LEVEL_INFO  1
-#define LOG_LEVEL_WARN  2
-#define LOG_LEVEL_ERROR 3
-
-extern int g_log_level;
-static FILE* g_log_file = NULL;
-static pthread_mutex_t log_mutex; //保证如果往文件输出的话是线程安全的
+int g_log_level;
+FILE* g_log_file = NULL;
+pthread_mutex_t log_mutex;
 
 void init_log_file(const char* filepath) {
     pthread_mutex_init(&log_mutex, NULL);
@@ -62,10 +51,3 @@ void close_log_file() {
     }
     pthread_mutex_destroy(&log_mutex);
 }
-
-#define LOG_DEBUG(message, ...) log_message(LOG_LEVEL_DEBUG, __FILE__, __LINE__, __FUNCTION__, message, ##__VA_ARGS__)
-#define LOG_INFO(message, ...)  log_message(LOG_LEVEL_INFO, __FILE__, __LINE__, __FUNCTION__, message, ##__VA_ARGS__)
-#define LOG_WARN(message, ...)  log_message(LOG_LEVEL_WARN, __FILE__, __LINE__, __FUNCTION__, message, ##__VA_ARGS__)
-#define LOG_ERROR(message, ...) log_message(LOG_LEVEL_ERROR, __FILE__, __LINE__, __FUNCTION__, message, ##__VA_ARGS__)
-
-#endif
